f22441128dfe6ca30ac815e4055be93f68b6458b
viniciusgf2 <viniciusgf@me.com>
2020-09-02 20:04:32 -0300

    Changed mod version

--- src/main/java/vazkii/ambience/Reference.java
+++ src/main/java/vazkii/ambience/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	public static final String MOD_ID = "ambience";
 	public static final String MOD_NAME = MOD_ID;
 	public static final String BUILD = "0";
-	public static final String VERSION = "1.5"+BUILD;
+	public static final String VERSION = "1.5."+BUILD;
 	public static final String DEPENDENCIES = "";
 	public static final String CLIENT ="vazkii.ambience.proxy.ClientProxy";
 	public static final String COMMON ="vazkii.ambience.proxy.CommonProxy";
